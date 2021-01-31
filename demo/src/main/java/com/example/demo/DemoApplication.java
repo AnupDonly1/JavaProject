48d80255ed67f0dbd943ebfa58b7526868a9155b
anup <anupthakur1495@gmail.com>
2021-01-31 12:32:06 -0800

    modifying logics

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.example.demo.controller.DemoController;
-
 @SpringBootApplication
 @ComponentScan({"com.example.*"})//basePackageClasses = DemoController.class)
 public class DemoApplication {
